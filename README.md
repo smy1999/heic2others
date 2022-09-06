188ace33404131dfb08079270bb53d5f1e57c907
smy1999 <865988027@qq.com>
2022-09-06 15:56:22 +0800

    Update

---
+++ README.md
@@ -0,0 +1,13 @@
+# HEIC2OTHERS
+***
+Browser-side conversion tool of HEIC image files to PNG, JPG or JPEG.
+
+Click [here](https://smy1999.github.io/heic2others/) to have a quick start.
+
+### 优化方向
+- [ ]  文件批量下载
+- [ ]  前端界面优化
+- [ ]  稳定性增强
+
+***
+Also welcome to [my blog](https://smy1999.github.io)
--- convert.js
+++ convert.js
@@ -1,54 +1,28 @@
 import heic2any from "./node_modules/heic2any/dist/heic2any.js";
 
-console.log("12312")
+console.log("Loading success");
 
 export function convert() {
 
-    console.log("456")
+    // alert("Start converting");
+    console.log("Start converting");
 
     const para_rate = parseFloat(document.getElementById("rate").value);
     const para_format = document.getElementById("file_format").value;
     const para_files_list = document.getElementById("files").files;
-    const para_path = document.getElementById("path").value;
-    console.log(para_files_list);
-
-    console.log(para_rate);
-    console.log(para_format);
-    console.log("789");
 
     for (let i = 0; i < para_files_list.length; i++) {
         const para_file = para_files_list[i].name;
-        // const para_path = "./" + para_file;
         const file_name = get_file_name(para_file);
         const file_type = get_file_type(para_format);
-        // console.log(file_name + " + " + file_type);
 
-        // fetch(para_path)
-        let file_full_path;
-        if (para_path[para_path.length - 1] != '/')
-           file_full_path = para_path + "/" + para_file
-        else
-            file_full_path = para_path + para_file
-        console.log("parafile: " + file_full_path);
-
-
-
-
-
-
-
-        fetch(file_full_path)
-            .then((res) => res.blob())
-            .then((blob) =>
-                heic2any({
-                    blob,
-                    toType: para_format,
-                    quality : para_rate
-                })
-            )
+        const conversionResult =  heic2any({
+            blob: para_files_list[i],
+            toType: para_format,
+            quality : para_rate
+        });
+        conversionResult
             .then((conversionResult) => {
-                // conversionResult is a BLOB
-                // of the PNG formatted image
                 let a = document.createElement("a");
                 a.href = URL.createObjectURL(conversionResult);
                 a.download = file_name + "." + file_type;
@@ -57,9 +31,7 @@ export function convert() {
                 a.click();
                 URL.revokeObjectURL(a.href); // 释放URL 对象
                 a.remove();
-            })
-            .catch((e) => {
-                // see error handling section
+            }).catch((e) => {
                 console.log(e);
             });
     }
